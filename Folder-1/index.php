07777081d04e89584e83252e208aade1637955a9
arbabmujtaba <arbabandjones@gmail.com>
2026-02-09 16:35:31 +0000

    MY PHP

---
+++ Folder-1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <div class="container">
+        This is my first php website That i build
+        <?php
+        echo "This is my php";
+        $var1=244;
+        $var2=22;
+        echo $var1 + $var2 
+        ?>
+    </div>
+</body>
+</html>
